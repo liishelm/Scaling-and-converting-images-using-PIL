979a7cc3769f2bf3b2571786725abf2e7d6173e9
liishelm <123267099+liishelm@users.noreply.github.com>
2023-12-16 17:03:53 +0200

    Update convert_img.py

--- convert_img.py
+++ convert_img.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+directory = 'images/'
+new_directory = '/opt/icons/'
+for file in os.listdir(directory):
+  f = os.path.join(directory, file)
+  if .DS_Store  not in f:
+    with Image.open(f) as img:
+     img = img.convert('RGB')
+     img = img.rotate(-90)
+     img = img.resize((128,128))
+     img.save(os.path.join(new_directory, file + '.jpeg'))
 
